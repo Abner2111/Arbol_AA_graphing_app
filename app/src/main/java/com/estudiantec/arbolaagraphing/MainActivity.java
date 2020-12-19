324af70e76a04c5cc392a96674c26f36b0f8d5c4
Abner2111 <38987646+Abner2111@users.noreply.github.com>
2020-12-19 00:13:47 -0600

    add functionality to MainActivity buttons. (Missing complete functionality of view tree button)

--- app/src/main/java/com/estudiantec/arbolaagraphing/MainActivity.java
+++ app/src/main/java/com/estudiantec/arbolaagraphing/MainActivity.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import com.estudiantec.estructura.*;
 
 public class MainActivity extends AppCompatActivity {
 
     AATree tree = new AATree();
+    TextView nodeEditText = (TextView) findViewById(R.id.nodeEditText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,29 @@ public class MainActivity extends AppCompatActivity {
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                int nodeData = Integer.parseInt(nodeEditText.getText().toString());
+
+                tree.insert(nodeData);
+            }
+        });
+
+        Button deleteBtn = (Button) findViewById(R.id.deleteBtn);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nodeData = Integer.parseInt(nodeEditText.getText().toString());
+
+                tree.remove(nodeData);
+
+            }
+        });
+
+        Button searchBtn = (Button) findViewById(R.id.searchBtn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nodeData = Integer.parseInt(nodeEditText.getText().toString());
+                tree.find(nodeData);
             }
         });
     }
